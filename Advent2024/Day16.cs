ba39bc8e2e6ac5ae3716e33dbdf75b9fe422a06d
Adam Nash <NashA4@michigan.gov>
2024-12-29 12:22:02 -0500

    Use GridHelper instead of ValueTuples

--- Advent2024/Day16.cs
+++ Advent2024/Day16.cs
@@ -5,7 +5,7 @@ public class Day16(InputHelper inputHelper) : IAdventPuzzle
     private readonly record struct Tile(int X, int Y, int Dir)
     {
         private static readonly (int x, int y)[] directions = [(1, 0), (0, -1), (-1, 0), (0, 1)];
-        public readonly (int, int) XY => (X, Y);
+        public readonly int[] XY => [Y, X];
         public readonly IEnumerable<(Tile tile, int score)> Next(int score) => [
             (this with { X = X + directions[Dir].x, Y = Y + directions[Dir].y }, score + 1),
             (this with { Dir = (Dir+1)%4 }, score + 1000),
@@ -19,31 +19,28 @@ public class Day16(InputHelper inputHelper) : IAdventPuzzle
 
     public long Solve(bool isPart1)
     {
-        var grid = inputHelper.EachLine(line => line)
-            .SelectMany((line, y) => line.Select((c, x) => (c, x, y)))
-            .GroupBy(t => t.c, t => (t.x, t.y))
-            .ToDictionary(g => g.Key, g => g.ToHashSet());
-        var (start, goal) = (grid['S'].Single(), grid['E'].Single());
+        var grid = new Cartesian<int>.GridHelper(inputHelper.EachLine(line => line));
+        var start = grid['S'].Single();
 
         var reachable = new Dictionary<Tile, int>();
-        var search = new PriorityQueue<Tile, int>([(new(start.x, start.y, 0), 0)]);
+        var search = new PriorityQueue<Tile, int>([(new(start[1], start[0], 0), 0)]);
 
         var best = int.MaxValue;
         while (search.TryDequeue(out var tile, out var score) && score <= best)
         {
-            if (tile.XY == goal)
+            if (grid[tile.XY] == 'E')
             {
                 if (isPart1) return score;
                 best = score;
             }
-            if (!reachable.ContainsKey(tile) && !grid['#'].Contains(tile.XY))
+            if (!reachable.ContainsKey(tile) && grid[tile.XY] != '#')
             {
                 reachable[tile] = score;
                 search.EnqueueRange(tile.Next(score));
             }
         }
-        var bestPaths = new HashSet<(int x, int y)>();
-        var backtrack = new Queue<Tile>(reachable.Keys.Where(s => s.XY == goal));
+        var bestPaths = new HashSet<int[]>(grid);
+        var backtrack = new Queue<Tile>(reachable.Keys.Where(s => grid[s.XY] == 'E'));
         while (backtrack.TryDequeue(out var tile))
         {
             bestPaths.Add(tile.XY);
