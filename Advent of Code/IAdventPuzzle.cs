ac3909bf139cdd5f8aca4998560896a6e1a96638
Adam Nash <NashA4@michigan.gov>
2025-01-04 18:55:14 -0500

    Static helper functions for reflecting Year and Day of a puzzle type

--- Advent of Code/IAdventPuzzle.cs
+++ Advent of Code/IAdventPuzzle.cs
@@ -4,9 +4,14 @@ public interface IAdventPuzzle
 {
     string Solve(InputHelper inputHelper);
 
-    static IEnumerable<Type> GetPuzzles(string annualNamespace) =>
+    public static IEnumerable<Type> GetPuzzles(string annualNamespace) =>
         Enumerable.Range(1, 25).Select(day => Type.GetType($"Advent_of_Code.{annualNamespace}.Day{day:D2}")).OfType<Type>();
 
-    static IAdventPuzzle Solver(Type type, int part) => Activator.CreateInstance(type, [part == 1]) as IAdventPuzzle
+    public static IAdventPuzzle Solver(Type type, int part) => Activator.CreateInstance(type, [part == 1]) as IAdventPuzzle
         ?? throw new ArgumentException($"Could not create {nameof(IAdventPuzzle)} from type {type}");
+
+    public static string Year(Type type) =>
+        type.IsAssignableTo(typeof(IAdventPuzzle)) ? type.Namespace?.Split('.')[^1][^4..] ?? string.Empty : string.Empty;
+    public static string Day(Type type) =>
+        type.IsAssignableTo(typeof(IAdventPuzzle)) ? type.Name[^2..] : string.Empty;
 }
