8ed4c5a3494761ca2f36c198007c02749141e66f
Adam Nash <NashA4@michigan.gov>
2025-01-04 18:54:15 -0500

    Change tests to use ClassData, to support other years

---
+++ AdventTest/AdventTests.cs
@@ -0,0 +1,28 @@
+﻿using Advent_of_Code;
+using Xunit;
+
+namespace AdventTest;
+
+public static class AdventTests
+{
+    [Theory]
+    [ClassData(typeof(AdventSolutions2022))]
+    [ClassData(typeof(AdventSolutions2024))]
+    public static void TestPuzzle(Type day, int part, string solution)
+    {
+        using var input = InputHelper.Create(day);
+        var puzzle = IAdventPuzzle.Solver(day, part);
+        Assert.Equal(solution, puzzle.Solve(input!));
+    }
+}
+
+public abstract class AdventSolutions : TheoryData<Type, int, string>
+{
+    protected abstract Dictionary<Type, IEnumerable<string>> Solutions { get; }
+    protected AdventSolutions() => AddRange(
+        Solutions.SelectMany(kvp => kvp.Value.Select((x, i) => new TheoryDataRow<Type, int, string>(kvp.Key, i + 1, x)
+            .WithTestDisplayName($"{IAdventPuzzle.Year(kvp.Key)}.{IAdventPuzzle.Day(kvp.Key)}.{i + 1}")
+            .WithTrait("Year", IAdventPuzzle.Year(kvp.Key))
+            .WithTrait("Day", IAdventPuzzle.Day(kvp.Key))
+            .WithTrait("part", $"{i + 1}"))));
+}
