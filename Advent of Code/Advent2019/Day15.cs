7083918f9780703dff005770127ccff0a6d0e057
Adam Nash <NashA4@michigan.gov>
2025-01-05 11:55:19 -0500

    Add Advent2019 stubs

---
+++ Advent of Code/Advent2019/Day15.cs
@@ -0,0 +1,9 @@
+﻿namespace Advent_of_Code.Advent2019;
+
+public class Day15(bool isPart1) : IAdventPuzzle
+{
+    public string Solve(InputHelper inputHelper)
+    {
+        return string.Empty;
+    }
+}
