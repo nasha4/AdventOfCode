83f764d48a5248b4e6f986f6db6949c61bdcf763
Adam Nash <NashA4@michigan.gov>
2025-01-30 23:24:19 -0500

    2023 stubs

---
+++ Advent of Code/Advent2023/Day06.cs
@@ -0,0 +1,9 @@
+﻿namespace Advent_of_Code.Advent2023;
+
+public class Day06(bool isPart1) : IAdventPuzzle
+{
+    public string Solve(InputHelper inputHelper)
+    {
+        return string.Empty;
+    }
+}
